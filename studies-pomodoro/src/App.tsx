b452869561cbfd79b3ef1e6d03bf738028cffcd0
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-05-30 11:51:50 -0300

    lista

--- studies-pomodoro/src/App.tsx
+++ studies-pomodoro/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Formulario from './components/Formulario/Formulario'
+import Formulario from './components/Formulario/Formulario';
+import Lista from './components/Lista/Lista';
 
 function App() {
   return (
     <div className="App">
       <Formulario/>
+      <Lista/>
     </div>
   );
 }
