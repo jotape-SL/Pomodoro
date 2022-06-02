c40fe3f340cc1699252a1bc3e8dafbdeb43721a5
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-06-01 21:48:27 -0300

    state

--- studies-pomodoro/src/components/Lista/Lista.tsx
+++ studies-pomodoro/src/components/Lista/Lista.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './Item/Item';
 import style from './Lista.module.scss';
 
 function Lista() {
-    const tarefas = [
+    const [tarefas, setTarefas] = useState([
         {
             tarefa: 'React',
             tempo: '02:00:00'
@@ -16,7 +16,7 @@ function Lista() {
             tarefa: 'Python',
             tempo: '03:30:00'
         }
-    ]
+    ]);
   return (
     <aside className={style.listaTarefas}>
         <h2>Estudos do dia</h2>
