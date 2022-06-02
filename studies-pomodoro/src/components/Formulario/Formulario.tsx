3805dff28fc48e234122fe907eede4c1258fd47e
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-06-02 10:13:21 -0300

    Form state

--- studies-pomodoro/src/components/Formulario/Formulario.tsx
+++ studies-pomodoro/src/components/Formulario/Formulario.tsx
@@ -3,15 +3,27 @@ import Botao  from '../Botao/Botao';
 import style from './Formulario.module.scss';
 
 export class Formulario extends React.Component {
+    
+    state = {
+        tarefa:"",
+        tempo:"00:00",        
+    };
+
+    adicionarTarefa(evento:React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault();
+    }
+
   render() {
     return (
-        <form className={style.novaTarefa}>
+        <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
             <div className={style.inputContainer}>
                 <label htmlFor="tarega">Adicione um novo estudo</label>
                 <input 
                     type="text"
                     name='tarefa'
                     id='tafera' 
+                    value={this.state.tarefa}
+                    onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}
                     placeholder='O que voce quer estudar'
                     required
                 />
@@ -23,6 +35,8 @@ export class Formulario extends React.Component {
                     step="1"
                     name='tepo'
                     id='tempo'
+                    value={this.state.tempo}
+                    onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
                     min="00:00:00"
                     max="01:30:00"
                     required
