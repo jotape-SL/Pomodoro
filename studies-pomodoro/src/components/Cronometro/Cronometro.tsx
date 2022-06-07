7d7153593e4f7b768478ea79b3b2fd7e24e4f761
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-06-07 10:08:49 -0300

    Timer dynamicity
    when clicked on the task it show the time you added on the clock

--- studies-pomodoro/src/components/Cronometro/Cronometro.tsx
+++ studies-pomodoro/src/components/Cronometro/Cronometro.tsx
@@ -2,7 +2,7 @@ import Botao from "../Botao/Botao"
 import Relogio from "./Relogio/Relogio"
 import style from './Cronometro.module.scss';
 import { Itarefa } from "../../types/tarefas";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { tempoParaSegundos } from "../../common/utils/time";
 
 interface Props{
@@ -11,11 +11,16 @@ interface Props{
 
 function Cronometro({selecionado}: Props) {
   const [tempo, setTempo] = useState<number>(tempoParaSegundos(String(selecionado?.tempo)));
+  useEffect(() =>{
+    if(selecionado?.tempo){
+      setTempo(tempoParaSegundos(selecionado.tempo))
+    };
+  },[selecionado])
   return (
     <div className={style.cronometro}>
         <p className={style.titulo}>Escolha um card e inicie o cronômetro.</p>
         <div className={style.relogioWrapper}>
-            <Relogio/>
+            <Relogio tempo={tempo} />
         </div>
         <Botao>
             Começar!
