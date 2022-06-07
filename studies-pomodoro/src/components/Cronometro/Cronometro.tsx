23c1a030a2c2cc36d3ab710ef6d6d8edbb22109b
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-06-07 05:57:30 -0300

    Start rendering Cronometro

--- studies-pomodoro/src/components/Cronometro/Cronometro.tsx
+++ studies-pomodoro/src/components/Cronometro/Cronometro.tsx
@@ -1,8 +1,16 @@
 import Botao from "../Botao/Botao"
 import Relogio from "./Relogio/Relogio"
 import style from './Cronometro.module.scss';
+import { Itarefa } from "../../types/tarefas";
+import { useState } from "react";
+import { tempoParaSegundos } from "../../common/utils/time";
 
-function Cronometro() {
+interface Props{
+  selecionado: Itarefa | undefined
+}
+
+function Cronometro({selecionado}: Props) {
+  const [tempo, setTempo] = useState<number>(tempoParaSegundos(String(selecionado?.tempo)));
   return (
     <div className={style.cronometro}>
         <p className={style.titulo}>Escolha um card e inicie o cronômetro.</p>
