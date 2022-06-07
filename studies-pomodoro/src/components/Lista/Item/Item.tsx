c573ac2d1a27cf7bb7de3751e22833a75596c5a6
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-06-07 05:35:42 -0300

    changed Item style and finished selected item visuals

--- studies-pomodoro/src/components/Lista/Item/Item.tsx
+++ studies-pomodoro/src/components/Lista/Item/Item.tsx
@@ -1,5 +1,5 @@
 import { Itarefa } from '../../../types/tarefas';
-import style from '../Lista.module.scss';
+import style from './Item.module.scss';
 
 interface Props extends Itarefa {
   selecionaTarefa: (tarefaSelecionada: Itarefa) => void 
@@ -8,7 +8,7 @@ interface Props extends Itarefa {
 export default function Item({tarefa, tempo, selecionado, completado, id, selecionaTarefa }: Props) {
   return (
     <li 
-    className={style.item}
+    className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
     onClick = {()=> selecionaTarefa(
     {
       tarefa,
